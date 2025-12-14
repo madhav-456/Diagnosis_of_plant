f402feebb9dfad0ce9a7af242f140f329045782f
Madhavan Thiruvayipati <madhavanthiruvaipati@gmail.com>
2025-12-14 10:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,4 +207,6 @@ def api_subsidies():
 
 # -------------------- START --------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
